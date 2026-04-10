7abf7252967ec052ccb720e8bd78a7b778913e75
Soulama Noe <soulamanoepro3@gmail.com>
2026-04-09 23:49:52 -0400

    project added

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,28 +3,96 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{username:"noe",password:"123"}];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+const existingUsers = users.filter((user)=>user.username === username)
+return existingUsers.length > 0
 }
+
+
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+const existingUsers = users.filter((user)=>user.username ===username && user.password === password)
+return existingUsers.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username =req.body.username;
+  const password = req.body.password;
+  if (!authenticatedUser(username,password)) {
+    return res.status(403).json({ message: "Invalid username or password" });
+  }
+  try {
+    const token = jwt.sign({data:username},"fingerprint_customer",{expiresIn:'1h'});
+    req.session.authorization = {
+        token:token,
+        username:username
+    }
+    return res.status(200).json({message :"Login successfully"})
+  } catch (error) {
+    return res.status(404).send("An error occured",error);
+  }
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (!isbn) {
+    return res.status(404).send("no params")
+  }
+  
+    const review = req.body.reviews;
+    if (!review) {
+        return res.status(404).send("Nothing to add")
+    }
+    const username = req.session.authorization ? req.session.authorization.username :null
+  try {
+    books[isbn].reviews[username]=review;
+   
+    return res.status(200).json(books[isbn]);
+  } catch (error) {
+    return res.status(500).send("an error occured",error)
+  }
+ 
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization ? req.session.authorization.username : null;
+  
+    if (!isbn) {
+      return res.status(400).send("ISBN is required");
+    }
+    if (!username) {
+      return res.status(403).send("User not logged in");
+    }
+  
+    try {
+
+      if (books[isbn] && books[isbn].reviews[username]) {
+        
+        delete books[isbn].reviews[username];
+  
+        return res.status(200).json({
+          message: `Review for ISBN ${isbn} by user ${username} deleted.`,
+          reviews: books[isbn].reviews
+        });
+      } else {
+        return res.status(404).send("Review or Book not found");
+      }
+    } catch (error) {
+      return res.status(500).send("An error occurred");
+    }
+  });
+
+  
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,37 +7,107 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password){
+    return res.status(404).send("All fields required")
+  }
+  userExisting = users.find((user)=>user.username === username);
+  if (userExisting) {
+    return res.status(404).send('this userName is already taken')
+  }
+  try {
+    const data = {username:username,password:password}
+    users.push(data);
+    return res.status(200).json(users)
+  } catch (error) {
+    return res.status(404).send("An error occured ",error)
+  }
+
+ 
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  return res.status(200).json(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn =req.params.isbn;
+  if (!isbn) {
+    return res.status(404).send("No params")
+  }
+  const book = books[isbn];
+  return res.status(200).json(JSON.stringify(book));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author =req.params.author;
+  if (!author) {
+    return res.status(404).send("No params")
+  }
+  try {
+    const book =[];
+    const decoder = decodeURIComponent(author).trim().toLowerCase();
+    for (const key in books) {
+        if (books[key]['author'].trim().toLowerCase()===decoder) {
+            book.push(books[key])
+            
+        }
+    }
+    return res.status(200).json(JSON.stringify(book))
+  } catch (error) {
+    return res.status(404).send("error occured:",error)
+  }
+ 
 });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+// Get all books based on isbn
+public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn =req.params.isbn;
+  if (!isbn) {
+    return res.status(404).send("No params")
+  }
+  try {
+    const book =[];
+    const decoder = decodeURIComponent(isbn).trim().toLowerCase();
+    for (const key in books) {
+        if (books[key]['isbn'].trim().toLowerCase()===decoder) {
+            book.push(books[key])
+            
+        }
+    }
+    return res.status(200).json(JSON.stringify(book))
+  } catch (error) {
+    return res.status(404).send("error occured:",error)
+  }
+ 
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn =req.params.isbn;
+  if (!isbn) {
+    return res.status(404).send("No params")
+  }
+  try {
+   
+    const decoder = decodeURIComponent(isbn).trim().toLowerCase();
+    const reviews = books[decoder]['reviews'];
+    
+    return res.status(200).json(JSON.stringify(reviews))
+  } catch (error) {
+    return res.status(404).send("error occured:",error)
+  }
+ 
 });
 
 module.exports.general = public_users;
